6c2fe25bd303c129b3b12b2d17b446b4fa9a848b
jesus <mateusmt885@gmail.com>
2023-12-21 08:21:11 -0300

    alterando metodo de criacao de pagina

--- blockpedia.sol
+++ blockpedia.sol
@@ -5,9 +5,15 @@ import "./pagina.sol";
 
 
 contract Blockpedia {
+    bool public ativo;
+    address public autor;
     Pagina[] public paginas;
-    
+
+    constructor(){autor = msg.sender; ativo = true;}
+
    function criarPagina(string memory titulo, string memory conteudo) public {
+        require(ativo == true, "A adicao de novas paginas so e permitida quando a Blockpedia esta ativa");
+        
         Pagina storage novaPagina = paginas.push();
         novaPagina.autor = msg.sender;
         novaPagina.ativo = true;
@@ -16,5 +22,9 @@ contract Blockpedia {
         novaPagina.versoes.push(Versao(msg.sender, true, conteudo));
     }
 
-
+    function ativaDesativaBlockpedia(bool _ativo) public
+    {
+        require(msg.sender == autor,"Somente o dono desta Blockpedia pode alterar o atributo ativo");
+        ativo = _ativo;
+    }
 }
