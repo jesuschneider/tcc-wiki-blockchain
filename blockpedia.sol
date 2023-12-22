e20c3ea180af7f4c65a3ca003d2a14726a75f566
jesus <mateusmt885@gmail.com>
2023-12-22 08:20:39 -0300

    adicionando verificacao de titulo existente

--- blockpedia.sol
+++ blockpedia.sol
@@ -3,28 +3,39 @@ pragma solidity 0.8.0;
 
 import "./pagina.sol";
 
-
 contract Blockpedia {
-    bool public ativo;
-    address public autor;
-    Pagina[] public paginas;
+    bool public ativo; 
+    address public autor; 
+    Pagina[] public paginas; 
 
-    constructor(){autor = msg.sender; ativo = true;}
+    constructor(){autor = msg.sender; ativo = true; }
 
    function criarPagina(string memory titulo, string memory conteudo) public {
         require(ativo == true, "A adicao de novas paginas so e permitida quando a Blockpedia esta ativa");
-        
-        Pagina storage novaPagina = paginas.push();
+        require(!tituloExiste(titulo), "Uma pagina com este titulo ja existe");
+
+        Pagina storage novaPagina = paginas.push();//cria nova pagina com ponteiro para o arry de paginas da blockpedia
         novaPagina.autor = msg.sender;
         novaPagina.ativo = true;
         novaPagina.titulo = titulo;
 
-        novaPagina.versoes.push(Versao(msg.sender, true, conteudo));
+        novaPagina.versoes.push(Versao(msg.sender, true, conteudo));//adiciona primeira vercao a pagina
     }
 
     function ativaDesativaBlockpedia(bool _ativo) public
     {
         require(msg.sender == autor,"Somente o dono desta Blockpedia pode alterar o atributo ativo");
         ativo = _ativo;
+    }
+
+    function tituloExiste(string memory titulo) internal view returns (bool)
+    {
+        //keccak256 é uma funcao hash, estou usando por conta que não tem comparacao de string em solidity
+        //abi.encodePacked() serve para tranformar a string em um "pacote" binario antes de transformar em hash
+        for (uint i = 0; i < paginas.length; i++)
+        {
+            if (keccak256(abi.encodePacked(paginas[i].titulo)) == keccak256(abi.encodePacked(titulo))) return true;
+        }
+        return false;
     }
 }
