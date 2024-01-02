aa93000adf4c637e53541907e2c600a85e106cd7
jesus <mateusmt885@gmail.com>
2024-01-02 12:28:57 -0300

    novos métodos para pegar primeira vercao ativa de uma pagina

--- blockpedia.sol
+++ blockpedia.sol
@@ -3,14 +3,16 @@ pragma solidity 0.8.0;
 
 import "./pagina.sol";
 
-contract Blockpedia {
-    bool public ativo; 
+contract Blockpedia
+{
+    bool public ativo = true; 
     address public autor; 
     Pagina[] public paginas; 
 
-    constructor(){autor = msg.sender; ativo = true; }
+    constructor(){autor = msg.sender;}
 
-   function criarPagina(string memory titulo, string memory conteudo) public {
+    function criarPagina(string memory titulo, string memory conteudo) public
+    {
         require(ativo == true, "A adicao de novas paginas so e permitida quando a Blockpedia esta ativa");
         require(!tituloExiste(titulo), "Uma pagina com este titulo ja existe");
 
@@ -31,11 +33,27 @@ contract Blockpedia {
     function tituloExiste(string memory titulo) internal view returns (bool)
     {
         //keccak256 é uma funcao hash, estou usando por conta que não tem comparacao de string em solidity
-        //abi.encodePacked() serve para tranformar a string em um "pacote" binario antes de transformar em hash
+        //abi.encodePacked() serve para transformar a string em um "pacote" binario antes de transformar em hash
         for (uint i = 0; i < paginas.length; i++)
         {
             if (keccak256(abi.encodePacked(paginas[i].titulo)) == keccak256(abi.encodePacked(titulo))) return true;
         }
         return false;
+    }
+
+    function getVersaoValidaPorIndexPaginas(uint indice) public view returns (string memory)
+    {
+        require(indice < paginas.length, "Indice fora do alcance");
+
+        return getVersaoValidaDeUmaPagina(paginas[indice]).conteudo;
+    }
+
+    function getVersaoValidaDeUmaPagina(Pagina memory pagina)internal pure returns (Versao memory)
+    {
+        for(uint i = 0; i < pagina.versoes.length; i++)
+        {
+            if(pagina.versoes[i].ativo) return pagina.versoes[i];
+        }
+        revert("Nenhuma versao valida encontrada");
     }
 }
